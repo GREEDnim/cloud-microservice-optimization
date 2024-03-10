3836a557315c65416ae308a0f3e7671de02782f6
Sivadhas <sivadhas007@gmail.com>
2024-03-10 19:51:35 +0530

    fix(exec) : added ant colony class

---
+++ trunk/src/main/Scheduler/AntColonyOptimizedScheduler.java
@@ -0,0 +1,50 @@
+package main.Scheduler;
+
+import main.Optimizer.RouteDiscovery;
+import main.services.Task;
+import org.cloudbus.cloudsim.Cloudlet;
+import org.cloudbus.cloudsim.CloudletSchedulerSpaceShared;
+import org.cloudbus.cloudsim.ResCloudlet;
+
+
+import java.util.*;
+
+public class AntColonyOptimizedScheduler extends CloudletSchedulerSpaceShared {
+
+    private RouteDiscovery ref ;
+    private HashMap<Integer,Integer>groupPriority ;
+
+    private Comparator<ResCloudlet> getComparator(){
+        Comparator<ResCloudlet> comp=new Comparator<>() {
+            @Override
+            public int compare(ResCloudlet o1, ResCloudlet o2) {
+                Task a=(Task) o1.getCloudlet();
+                Task b=(Task) o2.getCloudlet();
+
+                int aPriority=groupPriority.get(a.getGroupId());
+                int bPriority=groupPriority.get(b.getGroupId());
+
+                if(aPriority==bPriority) return (int)(a.getCloudletLength()-b.getCloudletLength());
+                return aPriority-bPriority;
+            }
+        };
+        return  comp;
+    }
+    public double cloudletSubmit(Cloudlet cloudlet, double fileTransferTime) {
+
+        this.ref = RouteDiscovery.getInstance();
+        this.groupPriority = ref.getPriorityMap();
+        Task task=(Task) cloudlet;
+        int gid=task.getGroupId();
+        if(!groupPriority.containsKey(gid)) groupPriority.put(gid,groupPriority.size());
+
+        return super.cloudletSubmit(cloudlet,fileTransferTime);
+    }
+    public double updateVmProcessing(double currentTime, List<Double> mipsShare) {
+
+        Collections.sort(getCloudletWaitingList(),getComparator());
+        return super.updateVmProcessing(currentTime,mipsShare);
+    }
+}
+
+
